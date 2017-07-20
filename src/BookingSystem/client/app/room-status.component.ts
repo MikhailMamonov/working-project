e49576ef7cccda0ab1bff08416d12f9cb236b85e
Alexey Gorishny <0x414c@gmail.com>
2017-07-20 17:31:18 +0300

    Add RoomStatus component (non-functioning yet)

---
+++ src/BookingSystem/client/app/room-status.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-room-status',
+
+  templateUrl: './room-status.component.html',
+
+  styleUrls: [
+    './room-status.component.css',
+  ],
+})
+export class RoomStatusComponent implements OnInit {
+  ngOnInit() { }
+}
