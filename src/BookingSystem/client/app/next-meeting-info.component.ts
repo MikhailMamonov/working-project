a4f25c3d74fc36a984f12062ba63afa69e9dabad
Alexey Gorishny <0x414c@gmail.com>
2017-07-20 17:31:34 +0300

    Add NextMeetingInfoComponent (non-functioning yet)

---
+++ src/BookingSystem/client/app/next-meeting-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-next-meeting-info',
+
+  templateUrl: './next-meeting-info.component.html',
+
+  styleUrls: [
+    './next-meeting-info.component.css',
+  ],
+})
+export class NextMeetingInfoComponent implements OnInit {
+  ngOnInit() { }
+}
