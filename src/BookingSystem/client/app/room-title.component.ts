5bbbff623710d323be827100cabd5cc9f0e8c4cf
Alexey Gorishny <0x414c@gmail.com>
2017-07-20 17:29:36 +0300

    Add RoomTitle component (non-functioning yet)

---
+++ src/BookingSystem/client/app/room-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-room-title',
+
+  templateUrl: './room-title.component.html',
+
+  styleUrls: [
+    './room-title.component.css',
+  ],
+})
+export class RoomTitleComponent implements OnInit {
+  ngOnInit() { }
+}
