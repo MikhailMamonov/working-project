1053f9cfb7ea8ab6dbe48061427d4b5e2d1881f7
Alexey Gorishny <0x414c@gmail.com>
2017-07-25 16:31:42 +0300

    Remove unused import

--- src/BookingSystem/client/app/components/server-connection-monitor/server-connection-monitor.component.ts
+++ src/BookingSystem/client/app/components/server-connection-monitor/server-connection-monitor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
