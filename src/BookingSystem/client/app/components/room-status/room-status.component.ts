b15f8dc4faafc4809dda656c4a90ac9aa56864ae
Alexey Gorishny <0x414c@gmail.com>
2017-07-25 16:35:14 +0300

    Add fully working RoomStatus component

--- src/BookingSystem/client/app/components/room-status/room-status.component.ts
+++ src/BookingSystem/client/app/components/room-status/room-status.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
+import * as moment from 'moment';
+
+import { Meeting } from '../../types/meeting';
 import { MeetingRoom } from '../../types/meeting-room';
 
 @Component({
@@ -14,17 +19,24 @@ import { MeetingRoom } from '../../types/meeting-room';
 export class RoomStatusComponent implements OnInit {
   @Input() room: MeetingRoom;
 
+  private readonly _tickPeriod = 1000 * 60; // 1m
+  remainingTimeStringObs: Observable<string>;
+
+  nextMeetingStartTime?: Date;
+
   isOccupiedNow = MeetingRoom.isOccupiedNow;
   hasNextMeeting = MeetingRoom.hasNextMeeting;
   currentMeeting = MeetingRoom.currentMeeting;
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.remainingTimeStringObs = Observable
+      .timer(0, this._tickPeriod)
+      .map(() => {
+        return moment().to(Meeting.endTime(MeetingRoom.currentMeeting(this.room)), true);
+      });
 
-  roomStatusText(): string {
-    if (this.isOccupiedNow(this.room)) {
-      return 'Occupied';
-    } else {
-      return 'Available';
+    if (MeetingRoom.hasNextMeeting(this.room)) {
+      this.nextMeetingStartTime = Meeting.startTime(MeetingRoom.nextMeeting(this.room)).toDate();
     }
   }
 }
