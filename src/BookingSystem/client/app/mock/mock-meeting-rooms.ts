818df288cf2f86da9f40c946f4e24506b1a22a69
Alexey Gorishny <0x414c@gmail.com>
2017-07-25 16:31:30 +0300

    Calculate dates and times automatically to simplify testing

--- src/BookingSystem/client/app/mock/mock-meeting-rooms.ts
+++ src/BookingSystem/client/app/mock/mock-meeting-rooms.ts
@@ -1,28 +1,35 @@
 // NOTE: We are using ISO 8601 for both times and durations.
-// TODO: [1;0] Calculate `startTime' from now to simplify testing.
+
+import * as moment from 'moment';
 
 export const MOCK_MEETING_ROOMS = [
-  // Now occupied, no next meeting:
+  // Occupied, no next meeting:
   {
     id: 1, name: 'Room #1 -- Foo', meetings: [
-      { startTime: '2017-07-25T12:34+03:00', duration: 'PT8H12M', user: 'John', description: 'Lorem ipsum' },
+      { startTime: moment().subtract(1, 'hour').toISOString(), duration: 'PT1H23M', user: 'John', description: 'Lorem ipsum' },
     ]
   },
-  // Now occupied, has next meeting
+  // Occupied, has next meeting:
   {
     id: 2, name: 'Room #2 -- Bar', meetings: [
-      { startTime: '2017-07-25T13:37+03:00', duration: 'PT1H23M', user: 'Jane', description: 'Dolor sit amet' },
-      { startTime: '2017-07-25T18:37+03:00', duration: 'PT2H23M', user: 'Jake', description: 'Consectetur adipiscing elit' },
+      { startTime: moment().subtract(1, 'hour').toISOString(), duration: 'PT1H23M', user: 'Jane', description: 'Dolor sit amet' },
+      { startTime: moment().add(4, 'hours').toISOString(), duration: 'PT2H34M', user: 'Jake', description: 'Consectetur adipiscing elit' },
     ]
   },
-  // Not occupied, data expired:
+  // Available, has next meeting:
+  {
+    id: 5, name: 'Room #5 -- Goo', meetings: [
+    { startTime: moment().add(1, 'hours').toISOString(), duration: 'PT1H23M', user: 'Jane', description: 'Dolor sit amet' },
+  ]
+  },
+  // Available, data expired:
   {
     id: 4, name: 'Room #4 -- Hoo', meetings: [
-      { startTime: '2017-01-25T13:37+03:00', duration: 'PT1H23M', user: 'Jane', description: 'Dolor sit amet' },
-      { startTime: '2017-01-25T18:37+03:00', duration: 'PT2H23M', user: 'Jake', description: 'Consectetur adipiscing elit' },
+      { startTime: '2017-01-25T12:34+03:00', duration: 'PT1H23M', user: 'Jane', description: 'Dolor sit amet' },
+      { startTime: '2017-01-25T16:20+03:00', duration: 'PT2H34M', user: 'Jake', description: 'Consectetur adipiscing elit' },
     ]
   },
-  // Not occupied, no meetings:
+  // Available, no meetings:
   {
     id: 3, name: 'Room #3 -- Baz'
   },
